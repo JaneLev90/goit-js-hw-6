bbe2e45ad1b50fd2f32dcd0f5972702f7baef16f
JaneLev90 <jlev888@gmail.com>
2023-02-12 17:46:15 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,17 +1,14 @@
 const text = document.querySelector('.color');
 const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
 
 btn.addEventListener('click', () =>{
-  // document.body.style.backgroundColor = Math.floor(Math.random() * 16777215)
-  //   .toString(16)
-  //   .padStart(6, 0);
- document.body.style.background = Math.floor(Math.random() * 16777215)
- .toString(16)
- .padStart(6, 0);
+ 
    text.textContent = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
  
+    body.style.background = text.textContent;
   });
 
 
