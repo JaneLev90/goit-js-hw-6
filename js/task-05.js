34e4e4cf476d5aec1f9559a2cd8a23098d3d663d
JaneLev90 <jlev888@gmail.com>
2023-02-13 18:00:13 +0200

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -11,25 +11,43 @@
 //    }
 //  });
 
+// const inputEl = document.querySelector("#name-input");
+// const userNameEl = document.querySelector("#name-output");
+
+// inputEl.addEventListener("input", function (event) {
+//   if (event.currentTarget.value) {
+//     userNameEl.textContent = event.currentTarget.value.trim();
+//   } else {
+//     userNameEl.textContent = event.currentTarget.value;
+//   }
+// });
+
+// inputEl.addEventListener("input", inputValueStart);
+
+// function inputValueStart () {
+// if (inputEl.value) {
+//   userNameEl.textContent = inputEl.value;
+// } else {
+//   userNameEl.textContent = "Anonymous";
+// }
+// }
+
 const inputEl = document.querySelector("#name-input");
 const userNameEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", function (event) {
+ 
+  if (!inputEl.value) {
+    return;
+  }
   if (event.currentTarget.value) {
     userNameEl.textContent = event.currentTarget.value.trim();
   } else {
     userNameEl.textContent = event.currentTarget.value;
   }
+
 });
 
-inputEl.addEventListener("input", inputValueStart);
 
-function inputValueStart () {
-if (inputEl.value) {
-  userNameEl.textContent = inputEl.value;
-} else {
-  userNameEl.textContent = "Anonymous";
-}
-}
 
 
