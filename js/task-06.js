2c0081920c5a30abb138240daf659ad9e4df7313
JaneLev90 <jlev888@gmail.com>
2023-02-13 15:23:59 +0200

    task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,21 +1,30 @@
 
- const inputEl = document.querySelector('#validation-input');
-inputEl.addEventListener('blur', changeBorder)
+//  const inputEl = document.querySelector("#validation-input");
+// inputEl.addEventListener("blur",  function  () {
+//   const dataLength = inputEl.getAttribute("data-length");
+// //  console.log(dataLength)
+//   const textLength = inputEl.value.length;
+//     //  console.log (textLength)
+//    if (dataLength === textLength){
+//    inputEl.classList.add("valid");
+//  }
+//   inputEl.classList.toggle("invalid");
+// })
  
-
-
- function changeBorder () {
-    const dataLength = inputEl.getAttribute("data-length");
-  // console.log(dataLength)
-    const textLength = inputEl.value.length;
-      // console.log (textLength)
- 
-    if (textLength != dataLength){
-   
-    inputEl.classList.add("invalid");
-   
- }
-
-    inputEl.classList.add("valid");
-  
-};
+const textInput = document.querySelector(`#validation-input`);
+let stringLength;
+textInput.addEventListener(`input`, onInputEvent);
+function onInputEvent(event) {
+  stringLength = event.currentTarget.value.length;
+  return stringLength;
+}
+textInput.addEventListener(`blur`, onInputBlur);
+function onInputBlur(event) {
+  if (stringLength !== Number(textInput.getAttribute(`data-length`))) {
+    textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
+  } else {
+    textInput.classList.add("valid");
+    textInput.classList.remove("invalid");
+  }
+}
