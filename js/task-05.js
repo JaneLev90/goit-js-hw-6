c8ca3a937368e57f4bf5b4fe50c14a4ce02a7423
JaneLev90 <jlev888@gmail.com>
2023-02-13 12:48:42 +0200

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,21 @@
-const inputEl = document.querySelector('#name-input');
-inputEl.addEventListener('input', onInputChange);
+// const inputEl = document.querySelector('#name-input');
+// inputEl.addEventListener('input', onInputChange);
 
-const outputEl = document.querySelector('span#name-output');
+// const outputEl = document.querySelector('span#name-output');
 
-function onInputChange (event) {
-    outputEl.textContent = event.currentTarget.value;
-    console.log (outputEl);
-}
+// function onInputChange (event) {
+//     outputEl.textContent = event.currentTarget.value;
+//     console.log (outputEl);
+// }
 
 
+const inputEl = document.querySelector("#name-input");
+const userNameEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", function (event) {
+  if (event.currentTarget.value.trim() !== "") {
+    userNameEl.textContent = event.currentTarget.value.trim();
+  } 
+    userNameEl.textContent = event.currentTarget.value;
+  
+});
